a7272ff18f27527f7c5ebc43fae1e5b189f8e15b
JohnnieKay12 <142480261+JohnnieKay12@users.noreply.github.com>
2025-03-11 15:27:53 +0100

    First Commit

--- components/HeaderSlider.jsx
+++ components/HeaderSlider.jsx
@@ -28,6 +28,22 @@ const HeaderSlider = () => {
       buttonText2: "Learn More",
       imgSrc: assets.header_macbook_image,
     },
+    {
+      id: 2,
+      title: "Next-Level Gaming Starts Here - Discover PlayStation 5 Today!",
+      offer: "Hurry up only few lefts!",
+      buttonText1: "Shop Now",
+      buttonText2: "Explore Deals",
+      imgSrc: assets.header_playstation_image,
+    },
+    {
+      id: 3,
+      title: "Power Meets Elegance - Apple MacBook Pro is Here for you!",
+      offer: "Exclusive Deal 40% Off",
+      buttonText1: "Order Now",
+      buttonText2: "Learn More",
+      imgSrc: assets.header_macbook_image,
+    },
   ];
 
   const [currentSlide, setCurrentSlide] = useState(0);
